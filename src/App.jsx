eb50705c976258237cf5cad2ed90120c53953482
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-26 23:27:42 -0700

    Add a background color to be able to see the border radius animation

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,10 @@ function App() {
         <p>HELLO</p>
       </motion.div>
       <motion.div
+        transition={{ duration: 2 }}
+        style={{ backgroundColor: 'orange' }}
         animate={{
-          scale: [1, 2, 2, 1, 1],
+          scale: [1, 2.2, 2.2, 1, 1],
           rotate: [0, 0, 270, 270, 0],
           borderRadius: ['20%', '20%', '50%', '50%', '20%'],
         }}>
