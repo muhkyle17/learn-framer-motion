f7d209262c5942c9d298f95f3bd4c5aabdb29bbc
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-26 23:49:44 -0700

    Add variant animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
+import { useState } from 'react'
 
 import { motion } from 'framer-motion'
 
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '-100%' },
+}
+
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className='division'>
       <motion.div
@@ -13,7 +21,7 @@ function App() {
       </motion.div>
       <motion.div
         transition={{ duration: 2 }}
-        style={{ backgroundColor: 'orange' }}
+        style={{ backgroundColor: 'orange', width: '50%' }}
         animate={{
           scale: [1, 2.2, 2.2, 1, 1],
           rotate: [0, 0, 270, 270, 0],
@@ -21,6 +29,10 @@ function App() {
         }}>
         <p>HI</p>
       </motion.div>
+      <motion.nav animate={isOpen ? 'open' : 'closed'} variants={variants}>
+        APPEAR DISAPPEAR
+      </motion.nav>
+      <button onClick={() => setIsOpen(isOpen => !isOpen)}>Toggle Click</button>
     </div>
   )
 }
